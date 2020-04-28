1e4919b835871751f664ceadc354f2ea4931815c
boureimatapily <boureimatapily1@gmail.com>
2020-04-28 12:45:55 +0000

    ADD ItemList and Iten Components

---
+++ Components/Item.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export class Item extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Item
