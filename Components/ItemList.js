2a9e7fa8c5921e64355c646d6a1c0b96cb75df5e
boureimatapily <boureimatapily1@gmail.com>
2020-04-28 12:46:17 +0000

    ADD ItemList component

---
+++ Components/ItemList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export class ItemList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> hello </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ItemList
