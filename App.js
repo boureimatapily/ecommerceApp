a5682b9afabba2fb7defc8a45de6792190649e08
boureimatapily <boureimatapily1@gmail.com>
2020-04-28 13:02:21 +0000

    ADD react-native-ratings

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Item from './Components/Item';
+import ItemList from './Components/ItemList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Item />
+     {/* <ItemList /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   marginTop:100
   },
 });
