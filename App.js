7c18578bcfc46323cf67a157b3c4c1c203e5d7cd
boureimatapily <boureimatapily1@gmail.com>
2020-04-28 23:08:01 +0000

    MOD content

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Item from './Components/Item';
 import ItemList from './Components/ItemList';
+import ItemLdetails from './Components/Itemdetails';
 
 export default function App() {
   return (
     <View style={styles.container}>
      {/* <Item /> */}
-     <ItemList />
+     {/* <ItemList /> */}
+     <ItemLdetails />
     </View>
   );
 }
